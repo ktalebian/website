621d3ef8fd6dd257e0ec8bff8fa6d9993c755b73
Kousha Talebian <kousha.talebian@gmail.com>
2025-07-08 00:05:23 -0700

    fix widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,9 @@ export default function Home() {
           </div>
         </div>
 
-        <ProjectsWidget />
+        <div className="hidden md:block">
+          <ProjectsWidget />
+        </div>
       </div>
 
       <Dock />
